bb86b2b5bbdfbb15bb364614e79ee6c8a629b5a5
Opeoluwa <olagokeopeolu1@gmail.com>
2023-07-01 19:15:11 +0100

    Move routes logic to a seperate file

---
+++ app/route.php
@@ -0,0 +1,33 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = __DIR__.'/../public/views/';
+
+switch ($request) {
+    case '':
+    case '/':
+        require $viewDir . 'home.php';
+        break;
+
+    case '/login':
+        require $viewDir . 'login.php';
+        break;
+
+    case '/contact':
+        require $viewDir . 'contact.php';
+        break;
+
+    case '/services':
+        require $viewDir . 'services.php';
+        break;
+
+    case '/about':
+        require $viewDir . 'about.php';
+        break;
+
+    case '/admin/dashboard':
+        require $viewDir . 'dashboard.php';
+        break;
+
+    default:
+        http_response_code(404);
+}
--- public/index.php
+++ public/index.php
@@ -1,40 +1,8 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../app/route.php';
 
 //Load .env file
 $dotenv = Dotenv\Dotenv::createImmutable('..', '.env');
 $dotenv->load();
-
-$request = $_SERVER['REQUEST_URI'];
-$viewDir = '/views/';
-
-switch ($request) {
-    case '':
-    case '/':
-        require __DIR__ . $viewDir . 'home.php';
-        break;
-
-    case '/login':
-        require __DIR__ . $viewDir . 'login.php';
-        break;
-
-    case '/contact':
-        require __DIR__ . $viewDir . 'contact.php';
-        break;
-
-    case '/services':
-        require __DIR__ . $viewDir . 'services.php';
-        break;
-
-    case '/about':
-        require __DIR__ . $viewDir . 'about.php';
-        break;
-
-    case '/admin/dashboard':
-        require __DIR__ . $viewDir . 'dashboard.php';
-        break;
-
-    default:
-        http_response_code(404);
-}

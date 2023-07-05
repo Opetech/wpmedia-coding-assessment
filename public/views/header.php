fe27a5fcd83f323e9a3df9aa1f0522d2c881d373
Opeoluwa <olagokeopeolu1@gmail.com>
2023-07-05 11:22:25 +0100

    Add sitemap link to navigation bar

--- public/views/header.php
+++ public/views/header.php
@@ -31,6 +31,9 @@ if (empty($meta_title)) {
             <li class="nav-item">
                 <a class="nav-link" href="/contact">Contact Us</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/sitemap">Sitemap</a>
+            </li>
         </ul>
     </div>
 </nav>
